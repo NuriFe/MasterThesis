023e0b567cd15cc49a9865f998f5b0fee14929c1
NuriFe <nurife.gva@gmail.com>
2023-07-10 17:14:50 +0200

    updated muscle torque

--- Kinematic Jacobian/torques_calculation.m
+++ Kinematic Jacobian/torques_calculation.m
@@ -3,17 +3,19 @@
 clc
 clear
 %% Specify the directory path
-directory = 'C:\Users\s202421\Documents/GitHub/MasterThesis\DataCreation\modelpoints/forces/brachialis';
+directory = 'C:\Users\s202421\Documents/GitHub/MasterThesis\DataCreation\modelpoints/forces/biceps';
 % Get a list of .mat files in the directory
 fileList = dir(fullfile(directory, '*.mat'));
 torques = zeros(11,length(fileList));
 configs = zeros(5,length(fileList));
 
+load('C:\Users\s202421\Documents\GitHub\MasterThesis\DataCreation\modelpoints\torques\torque_static.mat');
+
 for i = 1:length(fileList)
     % Load the .mat file
     filePath = fullfile(directory, fileList(i).name);
     data = load(filePath);
-    
+    static_torque = torques(:,i);
     % Access the data from the loaded .mat file
     hand_Fs=data.fs';%handF_total
     hand_F = hand_Fs(:,end);
@@ -26,12 +28,12 @@ for i = 1:length(fileList)
 
     [dPhand_dx, ~, ~] = handpos_jacobian(x);
     torque = dPhand_dx'*hand_F;
-    
-    torques(:,i)=torque;
+    muscle_torque = static_torque-torque;
+    torques(:,i)=muscle_torque;
     configs(:,i)=config;
 end
 
-name = append('C:\Users\s202421\Documents/Github/MasterThesis\DataCreation\modelpoints/torques/brachialis',string(i) );
+name = append('C:\Users\s202421\Documents/Github/MasterThesis\DataCreation\modelpoints/torques/biceps',string(i) );
 % Export to mat file and to opensim motion file
 save(name,'torques','config');
 fprintf('Data saved for torques')
--- STO/sto_creation.m
+++ STO/sto_creation.m
@@ -2,7 +2,7 @@
 clear;
 clc;
 % Some model related variables
-load('C:\Users\s202421\Documents\GitHub\MasterThesis\DataCreation\forces\brachialispoint14.mat')
+load('C:\Users\s202421\Documents\GitHub\MasterThesis\DataCreation\modelpoints\1.mat')
 modelparams = load('model_struct'); 
 model = modelparams.model;
 ndof = model.nDofs;
@@ -16,11 +16,11 @@ end
 %% Insert time start, end and time step
 t = 0;
 tstep = .003;
-tend = 1;
+tend = 3;
 nsteps = round((tend-t)/tstep);
 tout = tstep*(0:nsteps)';
-xout = xouts';
-file_name='brachialispoint14';
-name = append('C:\Users\s202421\Documents/Github/MasterThesis\DataCreation\sto/',file_name );
+xout = xout';
+file_name='staticpoint1';
+name = append('C:\Users\s202421\Documents/GitHub/MasterThesis\DataCreation\sto/',file_name );
 make_osimm(name, dofnames, xout(:,1:ndof), tout);
 fprintf('Simulation result for %s has been saved.\n ',file_name)
